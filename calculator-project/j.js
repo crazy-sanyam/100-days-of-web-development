26badeb4399039653a0e4045b3f6fc3c3c190296
Sanyam <sanyamjain9719@gmail.com>
2021-08-03 11:54:28 +0530

    day8: working on calculator project

--- calculator-project/j.js
+++ calculator-project/j.js
@@ -1,32 +1,32 @@
-
-function func11() {
-    //AC button
-    //this functin will clear all inputs
-    console.log("AC button clicked");
-    document.getElementById("inputBar").innerHTML = 0;
-}
-function func12() {
-    // */- button
-    //this function will change the sign 
-    var x;
-    x = document.getElementById("inputBar").innerHTML;
-    if (x < 0) {
-        x = x*(-1);
+function ID(clicked_id) {
+    id = clicked_id;
+    k = document.getElementById(id).innerHTML;
+    if (id == "b1" || id == "b2" || id == "b3" || id == "b4" || id == "b5" || id == "b6" || id == "b7" || id == "b8" || id == "b9" || id == "b0") {
+        l = document.getElementById("inputBar").innerHTML;
+        document.getElementById("inputBar").innerHTML = (10 * l + Number(k));
     }
-    document.getElementById("inputBar").innerHTML=x;
-
-}
-function func13() {
-    // % button
-    console.log("% button clicked");
-}
-function func14() {
-    // / button
-    console.log("/ button clicked");
-}
-function func24(){
-    // X button
-    var x,y;
-    x=document.getElementById("inputBar").innerHTML;
-    document.getElementById("inputBar").innerHTML=0;
+    if (id == "bAC") {
+        document.getElementById("inputBar").innerHTML = 0;
+        console.clear();
+    }
+    if (id == "bPlus" || id == "bMinus" || id == "bDivide" || id == "bMultiply") {
+        n1 = document.getElementById("inputBar").innerHTML;
+        a=id;
+        document.getElementById("inputBar").innerHTML=null;
+    }
+    if (id == "bEqual") {
+        n2=document.getElementById("inputBar").innerHTML;
+        if(a=="bPlus"){
+            document.getElementById("inputBar").innerHTML=Number(n1) + Number(n2);
+        }
+        if(a=="bMinus"){
+            document.getElementById("inputBar").innerHTML=Number(n1)-Number(n2);
+        }
+        if(a=="bMultiply"){
+            document.getElementById("inputBar").innerHTML=Number(n1)*Number(n2);
+        }
+        if(a=="bDivide"){
+            document.getElementById("inputBar").innerHTML=Number(n1)/Number(n2);
+        }
+        }
 }
