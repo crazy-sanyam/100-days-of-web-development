ae1228d2a047ad21c00a50876e07ea3a42beabb6
Sanyam <sanyamjain9719@gmail.com>
2021-08-05 13:37:18 +0530

    day10: now my calculator is also working with keyboard keys

--- calculator-project/j.js
+++ calculator-project/j.js
@@ -1,20 +1,35 @@
+window.addEventListener("keydown", f);
+function f(e) {
+    key1 = document.querySelector(`button[data-key="${e.keyCode}"]`);
+    // key2 = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    // alpha(key);
+    keyId1 = key1.getAttribute('id');
+    // keyId2 = key2.getAttribute('id');
+    alpha(keyId1);
+    // alpha(keyId2);
+}
 function ID(clicked_id) {
-    id = clicked_id;
+
+    // document.getElementById(clicked_id).style.;
+    alpha(clicked_id);
+}
+function alpha(apple) {
+    id = apple;
     k = document.getElementById(id).innerHTML;
     if (id == "b1" || id == "b2" || id == "b3" || id == "b4" || id == "b5" || id == "b6" || id == "b7" || id == "b8" || id == "b9" || id == "b0") {
         l = document.getElementById("inputBar").innerHTML;
         document.getElementById("inputBar").innerHTML = (10 * l + Number(k));
     }
-    if (id == "bAC") {
+    else if (id == "bAC") {
         document.getElementById("inputBar").innerHTML = 0;
         console.clear();
     }
-    if (id == "bPlus" || id == "bMinus" || id == "bDivide" || id == "bMultiply") {
+    else if (id == "bPlus" || id == "bMinus" || id == "bDivide" || id == "bMultiply") {
         n1 = document.getElementById("inputBar").innerHTML;
         a = id;
         document.getElementById("inputBar").innerHTML = null;
     }
-    if (id == "bEqual") {
+    else if (id == "bEqual") {
         n2 = document.getElementById("inputBar").innerHTML;
         if (a == "bPlus") {
             document.getElementById("inputBar").innerHTML = Number(n1) + Number(n2);
@@ -30,9 +45,15 @@ function ID(clicked_id) {
         }
 
     }
-    if (id == "bPercent") {
+    else if (id == "bPercent") {
         document.getElementById("inputBar").innerHTML = document.getElementById("inputBar").innerHTML / 100;
     }
+    else if (id == "backspace") {
+        gm = document.getElementById("inputBar").innerHTML;
+        gn = gm % 10;
+        gm = gm - gn;
+        gm = gm / 10;
+        document.getElementById("inputBar").innerHTML = gm;
+    }
 
-    
 }
