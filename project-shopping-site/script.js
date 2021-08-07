fc3cbdd861acc2f798ae39a11389c1cc3667a457
Sanyam <sanyamjain9719@gmail.com>
2021-08-07 12:48:11 +0530

    day11: working on project-Shopping Site

---
+++ project-shopping-site/script.js
@@ -0,0 +1,18 @@
+
+var shoes = 0, tShirt = 0, umbr = 0;
+function addToCart(clicked_id) {
+    console.log("hello");
+    switch (clicked_id) {
+        case "p1":
+            shoes++;
+            break;
+        case "p2":
+            tShirt++;
+            break;
+        case "p3":
+            umbr++;
+            break;
+
+    }
+    console.log(shoes + tShirt + umbr);
+}
