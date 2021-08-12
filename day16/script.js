64573914f84c94c7f0b389f686526ab21bd7f553
Sanyam <sanyamjain9719@gmail.com>
2021-08-12 15:46:45 +0530

    learning

---
+++ day16/script.js
@@ -0,0 +1,11 @@
+let age = 100;
+const age2 = age;
+// console.log(age, age2);
+age = 200;
+// console.log(age, age2);
+
+// let name = 'jain';
+// let name2 = name;
+// console.log(name, name2);
+// name = 'sam';
+// console.log(name, name2)
